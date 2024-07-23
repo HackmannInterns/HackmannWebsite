000411c1ab750a81eab7606d7f377c35401cc4c8
CraneLaptop <jaredchristophercastillo@callutheran.edu>
2024-07-23 16:18:29 -0700

    start working on main page

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write('File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 server.listen(port, hostname, () => {
