650befa8de8abe60c04c1091140fbf84cbf059be
areynen <alexreynen@gmail.com>
2024-07-24 15:21:16 -0700

    make the mobile viewports for nav

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,11 +1,29 @@
-var hider = document.querySelector("#nav-bar > p")
-var returner = document.querySelector("#flex-container > div:nth-child(1) > p")
-var navbar = document.querySelector("#nav-bar")
+var hider = document.querySelector("#nav-bar > p");
+var returner = document.querySelector("#flex-container > div:nth-child(1) > p");
+var navbar = document.querySelector("#nav-bar");
 function nav_hide() {
-  navbar.style.display = 'none'
-  returner.style.display = 'block'
-} 
+  navbar.style.display = "none";
+  returner.style.display = "block";
+}
 function nav_return() {
-  navbar.style.display = 'block'
-  returner.style.display = 'none'
-} 
+  navbar.style.display = "block";
+  returner.style.display = "none";
+}
+
+function hideNavBarOnSmallScreens() {
+  if (window.innerWidth <= 768) {
+    navbar.style.display = "none";
+    nav_hide();
+    hider.style.display = "block";
+} else {
+    navbar.style.display = "block"; // Reset display when window is larger than 768px
+    nav_return();
+    hider.style.display = "none";
+  }
+}
+
+// Run the function on window resize
+window.addEventListener("resize", hideNavBarOnSmallScreens);
+
+// Run the function on initial load
+hideNavBarOnSmallScreens();
