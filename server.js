fff7353436ae0d5793e3e86f80af389c742307dd
areynen <alexreynen@gmail.com>
2024-07-24 09:39:41 -0700

    move to views and ejs

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
-const { createServer } = require('node:http');
-
-const hostname = '0.0.0.0';
 const port = 3000;
 
-const express = require('express');
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
-  fs.readFile(path.join(__dirname,'index.html'), (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write('File not found!');
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-
+app.get("/", (req, res) => {
+  res.render("index", { title: "Hello World" });
 });
 
 app.listen(port, () => {
