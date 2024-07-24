a1644d8214b3dacd41986d110296a0c0faebb1be
CraneLaptop <jaredchristophercastillo@callutheran.edu>
2024-07-24 08:35:44 -0700

    made html work with the html file

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const { createServer } = require('node:http');
 const hostname = '0.0.0.0';
 const port = 3000;
 
+const express = require('express');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+  fs.readFile(path.join(__dirname,'index.html'), (err, data) => {
     if (err) {
       res.writeHead(404);
       res.write('File not found!');
@@ -19,8 +29,9 @@ const server = createServer((req, res) => {
     }
     res.end();
   });
+
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
